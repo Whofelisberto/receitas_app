8929087cf21fc923be5fa7d9f0df01a6f34100df
Whofelisberto <felisberto563@gmail.com>
2025-09-19 09:44:33 -0300

    fix: arrumando func jwtprotecj

--- backend/middlewares/auth.go
+++ backend/middlewares/auth.go
@@ -2,13 +2,13 @@
 package middlewares
 
 import (
-	"receitasfit/backend/utils"
+	"receitasfitness/backend/utils"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/jwt/v3"
+	jwtware "github.com/gofiber/jwt/v3"
 )
 
 func JWTProtected() fiber.Handler {
-	return jwt.New(jwt.Config{
+	return jwtware.New(jwtware.Config{
 		SigningKey:   utils.GetSecret(),
 		TokenLookup:  "cookie:token",
 	})
