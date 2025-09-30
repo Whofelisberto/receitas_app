3d726d141eff527a1012ded02ba92328e6ab7454
Whofelisberto <felisberto563@gmail.com>
2025-09-30 09:44:44 -0300

    feat: git add hash passowrd and jwt

---
+++ utils/hash.go
@@ -0,0 +1,18 @@
+// Package utils Funções utilitárias para hashing de senhas
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword Gera um hash bcrypt para a senha fornecida
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPasswordHash Compara a senha fornecida com o hash armazenado
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
