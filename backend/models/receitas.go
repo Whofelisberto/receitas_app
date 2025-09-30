f64f69e953dc67143986322fd82bd3ae149d8724
Whofelisberto <felisberto563@gmail.com>
2025-09-30 11:21:16 -0300

    fix: arrumar file receitas

--- backend/models/receitas.go
+++ backend/models/receitas.go
@@ -1,6 +1,7 @@
 //Package models Estrutura do modelo de dados para receitas
 package models
 
+
 type Receita struct {
 	ID          uint   `gorm:"primaryKey" json:"id"`
 	Title       string `json:"title"`
