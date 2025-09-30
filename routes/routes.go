8caf0d673d0787818898ae4ec231c52876b34690
Whofelisberto <felisberto563@gmail.com>
2025-09-30 09:46:41 -0300

    feat: add routes

---
+++ routes/routes.go
@@ -0,0 +1,19 @@
+// Package routes Rotas da aplicação
+package routes
+
+import (
+	"receitasfitness/backend/controllers"
+	"receitasfitness/backend/middlewares"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+	func Setup(app *fiber.App) {
+		app.Post("/registrar", controllers.Register)
+		app.Post("/login", controllers.Login)
+		app.Post("/receitas",middlewares.JWTProtected(), controllers.CreateReceita )
+
+		app.Get("/users", controllers.GetAllUsers)
+		app.Get("/users/:id", controllers.GetUserByID)
+		app.Get("/receitas", controllers.ListaReceitas )
+	}
